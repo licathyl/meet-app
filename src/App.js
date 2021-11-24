653ffd1264c57f0c7f3b154de0e692058737ee93
licathyl <83192907+licathyl@users.noreply.github.com>
2021-11-23 20:03:24 -0800

    add warning alert

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents } from './api';
+import { WarningAlert } from './Alert';
 
 class App extends Component {
   state = {
     events: [],
     locations: [],
     currentLocation: 'all',
-    numberOfEvents: 32
+    numberOfEvents: 32,
+    infoText: '',
   }
   
   componentDidMount() {
     this.mounted = true;
     getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
+      if(!navigator.onLine) {
+        if (this.mounted) {
+          this.setState({
+            infoText: 'Warning! No internet connection. The events displayed may not be up to date ', 
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events) 
+          });
+        }
+      } else {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events)
+          });
+        }
       }
     });
   }
@@ -51,6 +66,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <WarningAlert className="WarningAlert" text={this.state.infoText} />
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
         <EventList events={this.state.events} />
