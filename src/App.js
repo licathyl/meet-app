a3884ee4ac4e94b8dc21f7ccaec6b2ed9b50cb76
licathyl <83192907+licathyl@users.noreply.github.com>
2021-11-23 20:04:23 -0800

    add header

--- src/App.js
+++ src/App.js
@@ -66,6 +66,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Meet</h1>
         <WarningAlert className="WarningAlert" text={this.state.infoText} />
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateNumberOfEvents={this.updateNumberOfEvents} />
